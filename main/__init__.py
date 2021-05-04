0744a1cd346b045d421ef8bc464897459fecd99c
Maitreyee <maitreyee.das85@gmail.com>
2021-05-03 23:52:37 -0700

    update structure

--- main/__init__.py
+++ main/__init__.py
@@ -5,20 +5,21 @@ except ImportError:
     import werkzeug
     werkzeug.cached_property = werkzeug.utils.cached_property
     from flask_restplus import Resource, Api
-from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 import graphene
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
 from flask_graphql import GraphQLView
-from .config import config_by_name
 
-db = SQLAlchemy()
+from .config import config_by_name
+from .service.db_connect import db
+from .controller.user_controller import api as user_ns
+from .schema import schema
+
 flask_bcrypt = Bcrypt()
 api = Api(
     title='Lets Grow',
     version='1.0',
     description='API For Lets Grow',
-    # All API metadatas
 )
 
 def create_app(config_name):
@@ -28,14 +29,8 @@ def create_app(config_name):
     db.init_app(app)
     flask_bcrypt.init_app(app)
 
-    @app.route('/')
-    def index():
-        return '<p> Hello World!</p>'
-
-    from .controller.user_controller import api as user_ns
     api.add_namespace(user_ns, path='/user')
         
-    from .schema import schema
     app.add_url_rule(
         '/graphql',
         view_func=GraphQLView.as_view(
@@ -44,5 +39,9 @@ def create_app(config_name):
             graphiql=True # for having the GraphiQL interface
         )
     )
+    
+    @app.route('/')
+    def index():
+        return '<p> Hello World!</p>'
 
     return app
