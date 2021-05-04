62a75759f4eaf01572417da4dcbf06d3bdea17f3
Maitreyee <maitreyee.das85@gmail.com>
2021-05-04 16:35:00 -0700

    Adding the unit tests

---
+++ test/test_config.py
@@ -0,0 +1,48 @@
+import os
+import unittest
+
+from flask import current_app
+from flask_testing import TestCase
+
+from manage import app
+from main.config import basedir
+
+
+class TestDevelopmentConfig(TestCase):
+    def create_app(self):
+        app.config.from_object('main.config.DevelopmentConfig')
+        return app
+
+    def test_app_is_development(self):
+        self.assertFalse(app.config['SECRET_KEY'] == 'my_precious')
+        self.assertTrue(app.config['DEBUG'] == True)
+        self.assertFalse(current_app == None)
+        self.assertTrue(
+            app.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:///' + os.path.join(basedir, '../data.sqlite')
+        )
+
+
+class TestTestingConfig(TestCase):
+    def create_app(self):
+        app.config.from_object('main.config.TestingConfig')
+        return app
+
+    def test_app_is_testing(self):
+        self.assertFalse(app.config['SECRET_KEY'] == 'my_precious')
+        self.assertTrue(app.config['DEBUG'])
+        self.assertTrue(
+            app.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:///' + os.path.join(basedir, '../data.sqlite')
+        )
+
+
+class TestProductionConfig(TestCase):
+    def create_app(self):
+        app.config.from_object('main.config.ProductionConfig')
+        return app
+
+    def test_app_is_production(self):
+        self.assertTrue(app.config['DEBUG'] is False)
+
+
+if __name__ == '__main__':
+    unittest.main()
