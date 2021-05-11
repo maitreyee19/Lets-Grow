f4d6b3e26b92b32fa651977d34affda0a8be097d
Maitreyee <maitreyee.das85@gmail.com>
2021-05-10 21:41:25 -0700

    google OAuth

--- main/__init__.py
+++ main/__init__.py
@@ -11,10 +11,11 @@ from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
 from flask_graphql import GraphQLView
 
 from .config import config_by_name
-from .service.db_connect import db
+from .db_connect import db
 from .controller.user_controller import api as user_ns
 from .controller.post_controller import api as post_ns
 from .schema import schema
+from . import auth
 
 flask_bcrypt = Bcrypt()
 api = Api(
@@ -28,22 +29,23 @@ def create_app(config_name):
     api.init_app(app)
     app.config.from_object(config_by_name[config_name])
     db.init_app(app)
-    flask_bcrypt.init_app(app)
+    # flask_bcrypt.init_app(app)
 
+    app.register_blueprint(auth.bp)
     api.add_namespace(user_ns, path='/user')
     api.add_namespace(post_ns, path='/post')
-        
+
     app.add_url_rule(
         '/graphql',
         view_func=GraphQLView.as_view(
             'graphql',
             schema=schema,
-            graphiql=True # for having the GraphiQL interface
+            graphiql=True  # for having the GraphiQL interface
         )
     )
-    
+
     @app.route('/')
     def index():
         return '<p> Hello World!</p>'
-
+        
     return app
---
+++ main/auth.py
@@ -0,0 +1,85 @@
+# Third-party libraries
+from flask import Flask, redirect, request, url_for ,Blueprint, g , render_template, session
+import functools
+# from flask_login import (
+#     LoginManager,
+#     current_user,
+#     login_required,
+#     login_user,
+#     logout_user,
+# )
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+import json
+import os
+
+basedir = os.path.abspath(os.path.dirname("manage.py"))
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+def get_config():
+    with open( os.path.join(basedir, 'google-oidc.json'), 'r') as config_file:
+        config_data = json.load(config_file)
+        return config_data
+
+def get_google_provider_cfg():
+    return requests.get("https://accounts.google.com/.well-known/openid-configuration").json()
+
+
+config = get_config()
+
+# OAuth 2 client setup
+client = WebApplicationClient(config["client_id"])
+    # "1032839014332-44tte7j9ndhgl4hqtjaojhcq5sst6rse.apps.googleusercontent.com")
+
+
+
+
+@bp.route("/login")
+def login():
+
+    # Use library to construct the request for Google login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri("https://accounts.google.com/o/oauth2/auth",
+                                                redirect_uri=request.url_root + "auth/callback",
+                                                scope=[
+                                                    "openid", "email", "profile"],
+                                                )
+    return redirect(request_uri)
+
+@bp.route("/callback")
+def callback():
+    import os
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    # Get authorization code Google sent back to you
+    code = request.args.get("code")
+    # Prepare and send a request to get tokens! Yay tokens!
+    token_url, headers, body = client.prepare_token_request(
+        "https://oauth2.googleapis.com/token",
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(config["client_id"] , config["client_secret"])
+        # "1032839014332-44tte7j9ndhgl4hqtjaojhcq5sst6rse.apps.googleusercontent.com","dMl6H-26baE8LRjmi71S7dGv"),
+    )
+
+    # Parse the tokens!
+    client.parse_request_body_response(json.dumps(token_response.json()))
+    google_provider_cfg = get_google_provider_cfg()
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        users_name = userinfo_response.json()["given_name"]
+    else:
+        return( "User email not available or not verified by Google.", 400 )
+    print("unique_id {} users_email {} picture {} users_name {} ".format(
+        unique_id, users_email, picture, users_name))
+    return ("welcom {}".format(users_name) , 200)
